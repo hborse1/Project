7646a46ed3684b4cfc9ee7d96031afff7296ba7f
Borse <hemangi.borse@capgemini.com>
2020-11-19 19:06:01 +0530

    first commit

---
+++ ecommerce/controllers/product.js
@@ -0,0 +1,255 @@
+const formidable = require("formidable")
+const _ = require("lodash")
+const fs = require('fs')
+const Product = require('../models/product');
+const { result } = require("lodash");
+const  {errorHandler} = require("../helpers/dbErrorHandler");
+
+exports.productById = (req, res, next, id) => {
+    Product.findById(id)
+    .populate("category")
+    .exec((err, product) => {
+        if(err || !product){
+            return res.status(400).json({
+                error: 'Product not found'
+            });
+        }
+        req.product = product
+        next();
+    });
+};
+
+exports.read = (req, res) => {
+    req.product.photo = undefined
+    return res.json(req.product);
+};
+
+exports.create = (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            })
+        }
+
+        //check for all fields
+        const {name, description, price, category, quantity, shipping} = fields
+        if(!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "All fields are required"
+            });
+        }
+
+        let product = new Product(fields)
+        
+        if(files.photo) {
+            //console.log("Files photo: ",files.photo);
+            if(files.photo.size > 1000000 ) {
+                return res.status(400).json({
+                    error: "Image should be less than 1mb in size"
+                });
+            }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        }
+        product.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(result);
+        });
+    });
+};
+
+exports.remove = (req, res) => {
+    let product = req.product
+    product.remove((err, deleteProduct) => {
+        if(err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            message: "Product deleted successfully"
+        });
+    });
+};
+
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            })
+        }
+
+        //check for all fields
+        const {name, description, price, category, quantity, shipping} = fields
+        if(!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "All fields are required"
+            });
+        }
+
+        let product = req.product
+        product = _.extend(product, fields);
+        
+        if(files.photo) {
+            //console.log("Files photo: ",files.photo);
+            if(files.photo.size > 1000000 ) {
+                return res.status(400).json({
+                    error: "Image should be less than 1mb in size"
+                });
+            }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        }
+        product.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(result);
+        });
+    });
+};
+
+/**
+ * sell / arrival
+ * by sell = /product?sortBy=sold&order=desc&limit=4
+ * by arrival = /product?sortBy=createdAt&order=desc&limit=4
+ * if no params are sent, then all products are returned
+ */
+
+exports.list = (req, res) => {
+    let order = req.query.order ? req.query.order : 'asc'
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id'
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6
+
+    Product.find()
+    .select("-photo")
+    .populate('category')
+    .sort([[sortBy, order]])
+    .limit(limit)
+    .exec((err, data) => {
+        if(err) {
+            return res.status(400).json({
+                error: "Product not found"
+            });
+        }
+        res.json(data);
+    });
+}
+
+/**
+ * it will find products based on the req product category
+ * other products that has the same category, will be returned
+ */
+
+exports.listRelated = (req, res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6
+
+    Product.find({_id: {$ne: req.product}, category: req.product.category})
+    .limit(limit)
+    .populate('category', '_id name')
+    .exec((err, product) => {
+        if(err) {
+            return res.status(400).json({
+                error: "Product not found"
+            });
+        }
+        res.json(product);
+    });
+};
+
+exports.listCategories = (req, res) => {
+    Product.distinct("category", {}, (err, categories) => {
+        if(err) {
+            return res.status(400).json({
+                error: "Categories not found"
+            });
+        }
+        res.json(categories);
+    });
+};
+
+/**
+ * list products by search 
+ */
+
+exports.listBySearch = (req, res) => {
+    let order = req.query.order ? req.query.order : 'desc'
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id'
+    let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+    let skip = parseInt(req.body.skip);
+    let findArgs = {};
+
+    for(let key in req.body.filters) {
+        if(req.body.filters[key].length > 0) {
+            if(key == "price") {
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+    Product.find(findArgs)
+    .select("-photo")
+    .populate('category')
+    .sort([[sortBy, order]])
+    .skip(skip)
+    .limit(limit)
+    .exec((err, data) => {
+        if(err) {
+            return res.status(400).json({
+                error: "Products not found"
+            });
+        }
+        res.json({
+            size: data.length,
+            data
+        });
+    });
+};
+
+exports.photo = (req, res) => {
+    if(req.product.photo.data) {
+        res.set('Content-Type', req.product.photo.contentType)
+        return res.send(req.product.photo.data);
+    }
+    next();
+};
+
+exports.listSearch = (req, res) => {
+    //create query object to hold search value and category value
+    const query = {}
+    //assign search value to query.name
+    if(req.query.search) {
+        query.name = {$regex: req.query.search, $options: 'i'}
+        //assign category value to query.category
+        if(req.query.category && req.query.category != 'All') {
+            query.category = req.query.category
+        }
+        //find  the product based on query object with 2 properties
+        //search and category
+        Product.find(query, (err, products) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(products)
+        }).select("-photo")
+    }
+}
